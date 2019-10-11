a34992e9d2a195a01167712e46e24690bd21b60c
Syamsulandry <56153725+syamsulandry@users.noreply.github.com>
2019-10-11 17:32:13 +0700

    Tekanan Fluida Statis

--- main.py
+++ main.py
@@ -1,19 +1,5 @@
-nama = 'Syamsul Andry'
-program = 'Gerak Lurus'
-
-print(f'Program {program} oleh {nama}')
-
-def hitung_kecepatan(jarak, waktu):
-    kecepatan = jarak / waktu
-    print(f'Jarak = {jarak / 1000}km ditempuh dalam waktu = {waktu / 60}menit')
-    print(f'Sehingga kecepatan = {kecepatan} m/s')
-    return jarak / waktu
-
-# jarak = 1000
-# waktu = 5 * 60
-kecepatan = hitung_kecepatan(1000, 5 * 60)
-kecepatan = hitung_kecepatan(3000, 70 * 60)
-
 def hitung_tekanan_fluida(massa_jenis, percepatan_gravitas, ketinggian_fluida):
-    tekanan
+    tekanan_fluida = massa_jenis * percepatan_gravitas * ketinggian_fluida
+    print(f'Tekanan Fluida = {tekanan_fluida} Pascal')
 
+tekanan_fluida = hitung_tekanan_fluida(1, 10, 90)
